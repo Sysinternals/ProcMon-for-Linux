d65e7b2b08a7359bea6807b10f52f8d004adddf8
John Salem <josalem@microsoft.com>
2020-10-31 02:26:08 -0700

    building against 0.15 (#54)

--- src/display/screen.cpp
+++ src/display/screen.cpp
@@ -455,7 +455,7 @@ void Screen::run()
 
                 // handle mouse events
                 case KEY_MOUSE:
-                    if(getmouse(&mouseEvent) == OK) handleMouseEvent(&mouseEvent);
+                    if(getmouse(&mouseEvent) == NCURSES_OK) handleMouseEvent(&mouseEvent);
                     if(detailViewActive) showDetailView();
                     break;
                 
