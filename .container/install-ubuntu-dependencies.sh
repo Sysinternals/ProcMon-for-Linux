371c1cf51f9e6aecff7b6fe5429128321504efa5
Mario Hewardt <marioh@microsoft.com>
2024-04-09 08:22:26 -0700

    Updates

--- .container/install-ubuntu-dependencies.sh
+++ .container/install-ubuntu-dependencies.sh
@@ -51,6 +51,8 @@ sudo apt -y update && sudo apt-get install -y dotnet-runtime-6.0
 git clone --branch v0.19.0 https://github.com/iovisor/bcc.git
 mkdir -p bcc/build
 cd bcc/build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DLLVM_LIBRARY_DIRS=/usr/lib/llvm-10/bin
+sed -i 's/llvm-3\.7/llvm-6.0/g' ../CMakeLists.txt
+cat ../CMakeLists.txt
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DLLVM_LIBRARY_DIRS=/usr/lib/llvm-6.0/lib
 make
 sudo make install
